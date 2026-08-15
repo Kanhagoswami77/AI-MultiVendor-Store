b0a3d28aad2372596867936ef4575ad34e484d6d
Kanha Goswami <kanhagoswami822@gmail.com>
2026-08-15 15:40:25 +0530

    Fix database SSL for production

--- server/db.js
+++ server/db.js
@@ -3,6 +3,13 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
 });
 
 module.exports = pool;
